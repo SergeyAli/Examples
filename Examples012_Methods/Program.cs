867bc7ffef7f7bc41b503d04a274a5fb5647b9ab
«Sergey» <aliger99@rambler.ru>
2022-07-19 13:54:25 +0700

    Лекция про методы_for_Задача замены сисволов

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -72,14 +72,55 @@ string Method4(int count, string text)
 string res = Method4(10, "zv");
 Console.WriteLine(res);
 */
+
+/*
 for (int i = 2; i <=10; i++)
 {
     for (int j = 2; j <=10; j++)
     {
         Console.WriteLine($"{i}*{j} = {i*j}");
     }
-    Console.WriteLine();
+    Console.WriteLine(); //Искуственный разрыв десятков строк
+}
+*/
+
+// === Работа с текстом
+// Дан текст. В тексте нужно все заменить черточками,
+// маленькие буквы "к" заменить большими "К" ,
+//а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = " - Я думаю, - сказал князь, улыбаясь, - что "
+                + "ежели бы вас послали вместо нашего милого Винценгероде, "
+                + "вы бы взяли приступом согласие прусского короля. "
+                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345         
+//s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // иницилизация пустой строки
+
+    int length = text.Length; // определяем длинну строки
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}"; // если совпадает то положить в результат newValue
+        else result = result +  $"{text[i]}"; // иначе если совпададений не обноруженно положим в результат текуший символ
+    }
+
+    return result; // возвращаем результат
 }
 
 
+string newText = Replace(text, ' ', '|'); // заменяем пробелы на вертикальную строку
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
 
