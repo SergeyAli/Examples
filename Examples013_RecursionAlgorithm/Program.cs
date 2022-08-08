35dd84836f94381d9013c9d3f29177ba9f6f8707
Sergey <aliger99@rambler.ru>
2022-08-08 17:33:02 +0700

    Recursion Algorithm_Fibonacci

--- Examples013_RecursionAlgorithm/Program.cs
+++ Examples013_RecursionAlgorithm/Program.cs
@@ -47,51 +47,90 @@ for (int rows = 0; rows < 2; rows++)
 // System.Console.WriteLine();
 // PrintArray(matrix);
 
-int[,] pic = new int[,]
+
+// Работа с рисование
+
+
+// int[,] pic = new int[,]
+// {
+//     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//     {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//     {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+//     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+// };
+// void PrintImage(int[,] image) // подрограмма которая не выводить нули из масива , а единицы меняет на плюс
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             //System.Console.Write($"{image[i,j]} ");
+//             if (image[i,j] == 0) Console.Write($" ");
+//             else Console.Write($"+");
+//         }  
+//         System.Console.WriteLine();
+//     }
+// }
+// void FillImage (int row, int col) // подпраграмма закрашивания. аргументы - row - строка col - пиксел
+// {
+//     if (pic[row, col] ==0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row - 1, col);
+//         FillImage(row, col - 1);
+//         FillImage(row + 1, col);
+//         FillImage(row, col + 1);
+//     }
+// }
+
+
+// PrintImage(pic);
+// FillImage(10, 10);
+// PrintImage(pic);
+
+
+// Фактариал
+
+// double Factorial (int n) // если использовать тип данних int - то на больших числах произайде пререполнение
+//                          // поэтому используем тип double
+// {
+//     if(n==1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//   System.Console.WriteLine($"{i} {Factorial(i)}");  
+// }
+// //System.Console.WriteLine(Factorial(5));// фактариал 5!
+
+// Числа Фибоначи
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
 {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};
-void PrintImage(int[,] image) // подрограмма которая не выводить нули из масива , а единицы еняет на плюс
-{
-    for (int i = 0; i < image.GetLength(0); i++)
-    {
-        for (int j = 0; j < image.GetLength(1); j++)
-        {
-            //System.Console.Write($"{image[i,j]} ");
-            if (image[i,j] == 0) Console.Write($" ");
-            else Console.Write($"+");
-        }  
-        System.Console.WriteLine();
-    }
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n - 2);
 }
-void FillImage (int row, int col) // подпрагамма закрашивания аргументы - row - строка col - пиксел
+
+for (int i = 1; i < 50; i++)
 {
-    if (pic[row, col] ==0)
-    {
-        pic[row, col] = 1;
-        FillImage(row - 1, col);
-        FillImage(row, col - 1);
-        FillImage(row + 1, col);
-        FillImage(row, col + 1);
-    }
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
 
 
-PrintImage(pic);
-FillImage(10, 10);
-PrintImage(pic);
+
 
